d53349c3660e7237ebfe10cb1abcf09d25fd56fe
Miquel van Smoorenburg <mike@langeraar.net>
2022-11-09 00:06:30 +0100

    add list_movies

--- src/kodifs/movies.rs
+++ src/kodifs/movies.rs
@@ -1,4 +1,4 @@
-#[allow(dead_code)]
+use std::collections::HashMap;
 use std::path::PathBuf;
 use std::os::unix::fs::MetadataExt;
 
@@ -52,29 +52,49 @@ pub async fn update_movies(db: &DataBase, coll: &Collection, pace: u32) {
 }
 */
 
+// Get a list of all directories and their last-modified time (in unix ms)
+pub async fn list_movies(coll: &Collection, name: &str) -> HashMap<String, i64> {
+    let mut hm = HashMap::new();
+
+    let mut dirname = PathBuf::from(&coll.directory);
+    dirname.push(name);
+
+    let mut d = match fs::read_dir(&dirname).await {
+        Ok(d) => d,
+        Err(_) => return hm,
+    };
+
+    while let Ok(Some(entry)) = d.next_entry().await {
+        let file_name = entry.file_name();
+        let name = match file_name.to_str() {
+            Some(name) if !name.starts_with(".") && !name.starts_with("+ ") => name,
+            _ => continue,
+        };
+        if let Ok(modified) = entry.metadata().await.and_then(|m| m.modified()) {
+            hm.insert(name.to_string(), super::systemtime_to_ms(modified) as i64);
+        }
+    }
+    hm
+}
+
 pub async fn build_movie(coll: &Collection, name: &str, parse_nfo: bool) -> Option<Movie> {
 
     // First get all directory entries.
     let mut dirname = PathBuf::from(&coll.directory);
     dirname.push(name);
-    // println!("XXX {}/{} -> {:?}", coll.directory, name, dirname);
-
-    let mut d = match fs::read_dir(&dirname).await {
-        Ok(d) => d,
-        Err(_) => return None,
-    };
     let mut entries = Vec::new();
+    let mut d = fs::read_dir(&dirname).await.ok()?;
     while let Ok(Some(entry)) = d.next_entry().await {
         entries.push(entry);
     }
 
     // Collect timestamps.
-    let mut added_vec = Vec::new();
+    let mut added_ts = Vec::new();
     let meta = tokio::fs::metadata(&dirname).await.ok()?;
     let modified = meta.modified().ok()?;
-    added_vec.push(modified);
+    added_ts.push(modified);
     if let Ok(created) = meta.created() {
-        added_vec.push(created);
+        added_ts.push(created);
     }
 
     // Loop over all directory entries. We need to find a video file.
@@ -94,7 +114,7 @@ pub async fn build_movie(coll: &Collection, name: &str, parse_nfo: bool) -> Opti
         match entry.metadata().await {
             Ok(meta) => {
                 if let Ok(created) = meta.created() {
-                    added_vec.push(created);
+                    added_ts.push(created);
                 }
             },
             Err(_) => continue,
@@ -112,9 +132,9 @@ pub async fn build_movie(coll: &Collection, name: &str, parse_nfo: bool) -> Opti
     // TODO FIXME also use as fall-back title.
     let year = IS_YEAR.captures(name).map(|caps| caps[1].parse::<u32>().unwrap());
 
-    // `added_vec` contains a list of dates, use the oldest as `added`.
-    added_vec.sort();
-    // let added = (added_vec.len() > 0).then(|| DateTime<offset::Utc>::from(added_vec[0]));
+    // `added_ts` contains a list of dates, use the oldest as `added`.
+    added_ts.sort();
+    // let added = (added_ts.len() > 0).then(|| DateTime<offset::Utc>::from(added_ts[0]));
 
     // Initial Movie.
     let mut movie = Movie {
@@ -142,7 +162,7 @@ pub async fn build_movie(coll: &Collection, name: &str, parse_nfo: bool) -> Opti
         // poster.jpg
         match IS_EXT1.captures(name) {
             Some(caps) => {
-                ext = caps[3].to_string();
+                ext = caps[2].to_string();
                 if &caps[1] != base {
                     aux = caps[1].to_string();
                     if let Some(caps) = IS_EXT2.captures(name) {
