ba4605cb7a48ab718cd76e0d5fbf01906f1a4e5e
Miquel van Smoorenburg <mike@langeraar.net>
2022-11-19 13:22:51 +0100

    add models/mediainfo

---
+++ src/models/mediainfo.rs
@@ -0,0 +1,45 @@
+use anyhow::Result;
+use futures_util::TryStreamExt;
+use crate::db;
+use crate::models::Thumb;
+use crate::jvec::JVec;
+
+#[derive(Clone, Debug, sqlx::FromRow)]
+pub struct MediaInfo {
+    /// TVShow or Movie id
+    pub id: i64,
+    /// Title.
+    pub title: String,
+    /// Thumbnail in poster aspect (if available)
+    pub poster: Option<Thumb>,
+}
+
+impl MediaInfo {
+    pub async fn get_all(dbh: &db::DbHandle, collection_id: i64) -> Result<Vec<MediaInfo>> {
+        let mut rows = sqlx::query!(
+            r#"
+                SELECT i.id AS "id!: i64",
+                       i.title,
+                       i.thumbs AS "thumbs!: JVec<Thumb>"
+                FROM mediaitems i
+                WHERE i.collection_id = ?
+                ORDER BY LOWER( title)"#,
+            collection_id
+        )
+        .fetch(dbh);
+
+        let mut items = Vec::new();
+        while let Some(row) = rows.try_next().await? {
+            let poster = row.thumbs.0.iter().find(|t| t.aspect == "poster").cloned();
+            if let Some(title) = row.title {
+                items.push(MediaInfo {
+                    id: row.id,
+                    title,
+                    poster,
+                });
+            }
+        }
+
+        Ok(items)
+    }
+}
